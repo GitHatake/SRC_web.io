723020fc04bd2a6d636627c730a0ef553f8c87f0
HATAKEYAMA Yuki <a24117@numazu.kosen-ac.jp>
2025-10-29 18:39:01 +0900

    fix: for static

--- app/robots.ts
+++ app/robots.ts
@@ -1,5 +1,7 @@
 import { MetadataRoute } from 'next'
 
+export const dynamic = 'force-static'
+
 export default function robots(): MetadataRoute.Robots {
   const siteUrl = "https://githatake.github.io/SRC_web.io/"; // あなたのGitHub PagesのURLに変更してください
 
